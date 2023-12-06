50794baf6552783a67116b286158f01698f9b7ba
Bander <46300268+xZetsubou@users.noreply.github.com>
2023-12-07 01:32:48 +0300

    fix: update entry function

--- custom_components/localtuya/config_flow.py
+++ custom_components/localtuya/config_flow.py
@@ -604,6 +604,7 @@ class LocalTuyaOptionsFlowHandler(config_entries.OptionsFlow):
                     if CONF_MODEL not in dev and dev_id in cloud_devs:
                         model = cloud_devs[dev_id].get(CONF_PRODUCT_NAME)
                         new_data[CONF_DEVICES][dev_id][CONF_MODEL] = model
+
                 return self._update_entry(new_data, new_data.get(CONF_USERNAME))
 
             errors["base"] = res["reason"]
@@ -1084,6 +1085,14 @@ class LocalTuyaOptionsFlowHandler(config_entries.OptionsFlow):
             description_placeholders=placeholders,
         )
 
+    def _update_entry(self, new_data, new_title=""):
+        """Update entry data and save etnry,"""
+        _data = self.config_entry.data.copy()
+        _data.update(new_data)
+        _data[ATTR_UPDATED_AT] = str(int(time.time() * 1000))
+
+        self.hass.config_entries.async_update_entry(self.config_entry, data=_data)
+        return self.async_create_entry(title=new_title, data={})
 
     def available_dps_strings(self):
         """Return list of DPs use by the device's entities."""
