dc5e07fcd8c7d4c8b748e76e45b2b6194f289bde
Lurker00 <kis@aha.ru>
2024-07-24 11:43:35 +0300

    Nowhere to raise

--- custom_components/localtuya/core/pytuya/__init__.py
+++ custom_components/localtuya/core/pytuya/__init__.py
@@ -927,7 +927,7 @@ class TuyaProtocol(asyncio.Protocol, ContextualLogger):
                     await action()
                 except asyncio.CancelledError:
                     self.debug("Stopped heartbeat loop")
-                    raise
+                    break
                 except asyncio.TimeoutError:
                     self.debug("Heartbeat failed due to timeout, disconnecting")
                     break
