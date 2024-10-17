618139fc8d3e200423101454442791de2f491531
Lurker00 <Lurker00@users.noreply.github.com>
2024-10-18 02:45:47 +0300

    fix: is_connected property (#368)
    * Bug fix: self.transport shall not be None!
    
    * Simplify condition
    
    Co-authored-by: Bander <46300268+xZetsubou@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Bander <46300268+xZetsubou@users.noreply.github.com>

--- custom_components/localtuya/core/pytuya/__init__.py
+++ custom_components/localtuya/core/pytuya/__init__.py
@@ -1602,7 +1602,7 @@ class TuyaProtocol(asyncio.Protocol, ContextualLogger):
 
     @property
     def is_connected(self):
-        return not self.transport or not self.transport.is_closing()
+        return self.transport and not self.transport.is_closing()
 
     @property
     def last_command_sent(self):
